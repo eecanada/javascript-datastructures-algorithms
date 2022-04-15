936b6dfc175ddb44bcd529ec85037a4d0fa5633d
Eder Canada <canada.eder@gmail.com>
2022-04-14 23:02:10 -0700

    init

---
+++ arrays/index.js
@@ -0,0 +1,18 @@
+// Sorting objects
+const friends = [
+  { name: 'John', age: 30 },
+  { name: 'Ana', age: 20 },
+  { name: 'Chris', age: 25 }, // trailing comma ES2017
+];
+
+function comparePerson(a, b) {
+  if (a.age < b.age) {
+    return -1;
+  }
+  if (a.age > b.age) {
+    return 1;
+  }
+  return 0;
+}
+
+console.log('friends.sort(comparePerson)', friends.sort(comparePerson));
